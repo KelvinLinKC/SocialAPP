f1eca447c7b82e613a67f4f7d1ae277b18fc4f72
KelvinLinKC <2642614L@student.gla.ac.uk>
2022-12-30 09:22:38 +0800

    create google login function

--- SocialApp_frontend/src/index.js
+++ SocialApp_frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
 
 import App from './App'
 import './index.css'
 
+const root = createRoot(document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(
+    <Router>
+        <App />
+    </Router>
+);
